b33b25bedcb18488ff75732eb29fc73a6bb06c12
Cédric B <cedric@arch.localdomain>
2021-12-11 12:20:10 +0100

    d10: simpler stack-based solution

--- exe/d10.hs
+++ exe/d10.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import D10.Solution (solve)
+import qualified D10.Alt as Alt
 
 main :: IO ()
-main = getContents >>= solve
+main = do
+  input <- getContents
+
+  putStrLn "Main solution"
+  solve input
+
+  putStrLn "\nAlt solution"
+  Alt.solve input
---
+++ src/D10/Alt.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE LambdaCase #-}
+
+module D10.Alt (solve) where
+
+import Data.Either (lefts, rights)
+import Data.List (foldl', sort)
+
+-- the mismatched closing character
+type Error = Char
+
+-- the stack of expected closing characters
+type Stack = [Char]
+
+type ProcessState = Either Error Stack
+
+push :: Char -> ProcessState -> ProcessState
+push = fmap . (:)
+
+pop :: Char -> ProcessState -> ProcessState
+pop x c =
+  c >>= \case
+    x' : xs | x' == x -> return xs
+    _ -> Left x
+
+process :: [Char] -> ProcessState
+process = foldl' (flip op) (Right [])
+  where
+    op '[' = push ']'
+    op '(' = push ')'
+    op '<' = push '>'
+    op '{' = push '}'
+    op x = pop x
+
+scoreStack :: Stack -> Int
+scoreStack = foldl' (\s x -> 5 * s + closingScore x) 0
+  where
+    closingScore ')' = 1
+    closingScore ']' = 2
+    closingScore '}' = 3
+    closingScore '>' = 4
+    closingScore _ = 0
+
+scoreError :: Error -> Int
+scoreError = mismatchScore
+  where
+    mismatchScore ')' = 3
+    mismatchScore ']' = 57
+    mismatchScore '}' = 1197
+    mismatchScore '>' = 25137
+    mismatchScore _ = 0
+
+score :: ProcessState -> Int
+score = either scoreError scoreStack
+
+-- $> score . process $ "[({(<(())[]>[[{[]{<()<>>"
+
+-- $> score . process $ "[[<[([]))<([[{}[[()]]]"
+
+solve :: String -> IO ()
+solve input = do
+  print . solve1 . lines $ input
+  print . solve2 . lines $ input
+
+solve1 :: [String] -> Int
+solve1 = sum . fmap scoreError . lefts . fmap process
+
+solve2 :: [String] -> Int
+solve2 = median . fmap scoreStack . rights . fmap process
+  where
+    median :: Ord a => [a] -> a
+    median xs = sort xs !! (length xs `div` 2)
